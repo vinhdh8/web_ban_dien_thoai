8de67b28144d5892e1fc2c12386a4e239bf3372a
vinhdh8 <vinhdhph31804@fpt.edu.vn>
2024-07-27 16:29:57 +0700

    dmsp

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,4 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::get('/register', [AuthController::class, 'showFormRegister']);
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+//view
